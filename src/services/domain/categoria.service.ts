1dcc45ff3c606ef20f1143f0febc72c1093a32dc
sostenesantunes <sostenesantunes@gmail.com>
2020-11-05 23:57:33 -0300

    Listando Categorias parte-01

---
+++ src/services/domain/categoria.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class CategoriaService {
+
+  constructor (public http: HttpClient){
+
+  }
+
+  findAll() {
+
+  }
+}
