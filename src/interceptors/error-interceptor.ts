1801765d8852090d6722a104f853ec0a32386bd7
Sóstenes Antunes <sostenesantunes@gmail.com>
2020-11-15 23:24:59 -0300

    Tratando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,9 +1,13 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
+import { StorageService } from "../services/storage.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(public storage: StorageService) {
+  }
 
     intercept (req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
       return next.handle(req)
@@ -14,17 +18,27 @@ export class ErrorInterceptor implements HttpInterceptor {
         if (errorObj.error) {
           errorObj = errorObj.error;
         }
-        if (!errorObj.status) {
-          errorObj = JSON.parse(error);
-        }
+
+      /*   if (!errorObj.status) {
+          errorObj = JSON.parse(errorObj);
+        } */
 
         console.log("Erro detectado pelo interceptor:");
         console.log(errorObj);
 
-        return Observable.throw(error);
+        switch (errorObj.status) {
+          case 403:
+            this.handle403();
+            break;
+        }
+
+        return Observable.throw(errorObj);
       }) as any;
     }
 
+    handle403() {
+      this.storage.setLocalUser(null);
+    }
 }
 
 export const ErrorInterceptorProvider = {
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -40,8 +40,15 @@ export class ProfilePage {
         this.getImageIfExists();
         },
 
-        error => {}
-      );
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    }
+
+    else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
